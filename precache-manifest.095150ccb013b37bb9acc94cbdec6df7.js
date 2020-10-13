d701e9499585341b27021019d742f4b403c5137b
Kristuwa <kristuwa15@ukr.net>
2020-10-13 23:16:33 +0300

    Updates

---
+++ precache-manifest.095150ccb013b37bb9acc94cbdec6df7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69cffd7e5aa33f33b9a2ff274fa46fe1",
+    "url": "/birds/index.html"
+  },
+  {
+    "revision": "62ae4af4d5020bc9ff16",
+    "url": "/birds/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "48d37632f409523cf79a",
+    "url": "/birds/static/css/main.087f6758.chunk.css"
+  },
+  {
+    "revision": "62ae4af4d5020bc9ff16",
+    "url": "/birds/static/js/2.a47cc7f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birds/static/js/2.a47cc7f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48d37632f409523cf79a",
+    "url": "/birds/static/js/main.a68863b0.chunk.js"
+  },
+  {
+    "revision": "96ed373472f502a95fd0",
+    "url": "/birds/static/js/runtime-main.3abdc557.js"
+  },
+  {
+    "revision": "2f311a05a79fb86cee6148d658aa8336",
+    "url": "/birds/static/media/ITN_mix1.2f311a05.svg"
+  },
+  {
+    "revision": "66ba5fd651c7fbd92ca72be190a16cb2",
+    "url": "/birds/static/media/ecology.66ba5fd6.jpg"
+  },
+  {
+    "revision": "ebc2c1d9e824c6e1c84cd85a2a1a54a5",
+    "url": "/birds/static/media/heroBG.ebc2c1d9.jpg"
+  },
+  {
+    "revision": "341a487c2bb1cbed9179faa73d89b343",
+    "url": "/birds/static/media/shid_logo.341a487c.svg"
+  },
+  {
+    "revision": "15552c1c8287d125cdf6f243df6674e6",
+    "url": "/birds/static/media/technics.15552c1c.jpg"
+  }
+]);
